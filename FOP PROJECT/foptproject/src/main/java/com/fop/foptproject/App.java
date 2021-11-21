55e4b783279d7968c49a3a252361f8dcec535fbc
nwjun <63900029+nwjun@users.noreply.github.com>
2021-11-21 21:11:04 +0800

    Hide horizontal and vertical scroll bar

--- FOP PROJECT/foptproject/src/main/java/com/fop/foptproject/App.java
+++ FOP PROJECT/foptproject/src/main/java/com/fop/foptproject/App.java
@@ -10,6 +10,7 @@ import java.sql.SQLException;
 import java.util.Properties;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.ScrollPane.ScrollBarPolicy;
@@ -20,13 +21,20 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
+        // get computer screen size 
         javafx.geometry.Rectangle2D screenBounds = Screen.getPrimary().getBounds();
         double maxX = screenBounds.getMaxX();
-        double maxY = screenBounds.getMaxY()-30;
-//        double maxY = 1911.0;
-System.out.println(maxY);
+        double maxY = screenBounds.getMaxY();
+        
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("App.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(),maxX,maxY);
+        // set Scene with maximum width and height of screen
+        Scene scene = new Scene(fxmlLoader.load(),maxX,maxY); 
+        // get scrollPane from fxml
+        ScrollPane scrollPane = (ScrollPane) fxmlLoader.getRoot();
+        scrollPane.setPannable(false);
+        // hide horizontal and vertical scroll bar
+        scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+        scrollPane.setVbarPolicy(ScrollBarPolicy.NEVER);
         primaryStage.setTitle("Movie Ticketing System");
         primaryStage.setScene(scene);
         primaryStage.setResizable(false);
@@ -34,7 +42,7 @@ System.out.println(maxY);
     }
 
     public static void main(String[] args) throws Exception {
-        Properties prop = new readConfig().readconfigfile
+        // Properties prop = new readConfig().readconfigfile;
         //System.out.println(new emailTo(prop.getProperty("configuration.testSingleEmail")).sendBookingConfirmations("Eternals","forInternalTest","54321","12345","11/11/2021","1:30 PM","3 x Preferred (RM51.00 - E11, E10, E9)",52.5));
         //System.out.println(new emailTo(prop.getProperty("configuration.testSingleEmail")).sendTestMail());
         //System.out.println(new emailTo("limweixin17@gmail.com,helloworldisagurl@gmail.com").sendNotification("Eternals","11/11/2021","2:00pm","Kuala Lumpur - MidValley"));
