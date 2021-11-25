9318b7001204dc3282931e354e5459347cf68eb8
nwjun <63900029+nwjun@users.noreply.github.com>
2021-11-25 20:50:54 +0800

    Replace missing files

--- FOP PROJECT/foptproject/src/main/java/com/fop/foptproject/AppController.java
+++ FOP PROJECT/foptproject/src/main/java/com/fop/foptproject/AppController.java
@@ -22,6 +22,7 @@ import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
@@ -30,6 +31,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Line;
+import javafx.collections.ObservableList;
 
 public class AppController implements Initializable {
 
