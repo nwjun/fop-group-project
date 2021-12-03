c41162af1fada870a00cee44abded2730d5f49ac
KuckSN <93997126+KuckSN@users.noreply.github.com>
2021-12-03 21:08:08 +0800

    Designing and Admin Page Linking
    Add changeToAdminFood and changeToAdminMovie in SceneController
    
    Alter some design on Admin Pages

--- FOP PROJECT/foptproject/src/main/java/com/fop/foptproject/controller/AdminFoodController.java
+++ FOP PROJECT/foptproject/src/main/java/com/fop/foptproject/controller/AdminFoodController.java
@@ -3,11 +3,14 @@ package com.fop.foptproject.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
@@ -17,6 +20,8 @@ public class AdminFoodController implements Initializable {
 
     @FXML
     private ImageView DropImage;
+    @FXML
+    private Button backToMain;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -34,6 +39,12 @@ public class AdminFoodController implements Initializable {
         List<File> files = event.getDragboard().getFiles();
         Image img = new Image(new FileInputStream(files.get(0)));
         DropImage.setImage(img);
+    }
+
+    @FXML
+    private void switchToAdminMain(ActionEvent event) throws IOException {
+        SceneController SwitchScene = new SceneController();
+        SwitchScene.switchToAdminMain(event);
     }
     
 }
